aec87045fc007cdbe2a3269bc02cb5553155b711
Arji <rantow.arji@gmail.com>
2025-02-12 10:52:46 +0700

    Added new folders

--- Homeworks/LAB 01/Tugas1-Arji.py
+++ Homeworks/LAB 01/Tugas1-Arji.py
@@ -1,6 +1,5 @@
 def hangman(word):
     guesses = '_'
-    lives = 5
     while True:    
         failed = 0
         for char in word:
@@ -16,25 +15,10 @@ def hangman(word):
             break
 
         guess = input("Guess a character: ").lower()
-
-        if not guess.isalpha() or len(guess) != 1:
-            print("Please input a single letter at a time")
-            continue
-
-        if guess in guesses:
-            print("You already guessed that letter. Try another one")
-            continue
-
         guesses += guess
 
         if guess not in word:
-            lives -= 1
             print("Wrong the letter isn't in the word. Try Agains")
-            print("You have", lives, "more guesses")
-
-        if lives == 0:
-            print("You Lose")
-            break
-
 word = "sigma"
+
 hangman(word)
